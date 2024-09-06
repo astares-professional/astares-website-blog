be0c12569fb2ab16f0d458d1ca6dae13f9c9de80
Torsten Bergmann <astares@gmx.de>
2024-09-06 21:27:29 +0200

    brand colors

--- themes/tailwind/tailwind.config.js
+++ themes/tailwind/tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
         'heading': ['League Spartan', 'sans-serif'],
         'body': ['Poppins', 'sans-serif'],
       },
+      colors: {
+        'primary':   '#08305C',  // Very Dark Blue
+        'secondary': '#F39200',  // Saffron Gold
+        'accent1': '#FBF1E2',    // Fine Porcelaine
+        'accent2': '#D8E9F4',    // Azureish White
+        'accent3': '#DFDDE3',    // February Frost
+        'accent4': '#0A629E',    // Rave Regatta
+        'accent5': '#5494BF',    // Blue Paradise
+        'accent6': '#85B5D4',    // BLUE BELL 
+        'dark':    '#080808',    // Vampire Black
+        darkblue: {"50":"f8f9fc","100":"#f1f3f9","200":"#e2e6f2","300":"#cbd3e5","400":"#94a1c9","500":"#6472a0","600":"#47547a","700":"#33405d","800":"#1e2a3f","900":"#0f1627"}
+      },
       typography: {
         quoteless: {
           css: {
